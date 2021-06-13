f2c88333df1966f8f660a4113569ee93053c113b
Akramtello <35160721+AkramTT@users.noreply.github.com>
2021-06-13 21:51:36 +0800

    first dummy commit
    change to pandai
    
    dummy commit
    
    New Update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    pandai banget 
+     
+     <h1>Hello new update</h1>
+     <p>Lets Proceed with next staff</p>
+</body>
+</html>
