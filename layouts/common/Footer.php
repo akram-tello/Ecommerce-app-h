8d96bba94c1fc484c27a2d52c3b646e5dc83ada5
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-25 00:07:22 +0800

    added pagination components

--- layouts/common/Footer.php
+++ layouts/common/Footer.php
@@ -1,4 +1,5 @@
 <script src="../../assets/js/jquery2.js"></script>
 <script src="../../assets/js/bootstrap.min.js"></script>
+<script src="../../assets/js/main.js"></script>
 </body>
 </html>
---
+++ layouts/components/_pagination.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-12">
+        <center>
+            <ul class="pagination" id="pageno">
+                <li><a href="#">1</a></li>
+            </ul>
+        </center>
+    </div>
+</div>
