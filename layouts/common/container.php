3da074b944436818c211c34f9cffa68aa7b34176
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-24 18:59:44 +0800

    added container

---
+++ layouts/common/container.php
@@ -0,0 +1,26 @@
+<div class="container-fluid" style="margin-top: 5em;">
+    <div class="row">
+        <div class="col-md-2 col-xs-12">
+            <div id="get_category">
+            </div>
+            <div id="get_type_of_book">
+            <!--Here we get list type of books jquery Ajax Request-->
+            </div>
+        </div>
+        <div class="col-md-10 col-xs-12">
+            <div class="row">
+                <div class="col-md-12 col-xs-12" id="product_msg">
+                </div>
+            </div>
+            <div class="panel panel-info">
+                <div class="panel-heading">Books</div>
+                <div class="panel-body">
+                    <div id="get_product">
+                        <!--Here we get product jquery Ajax Request-->
+                    </div>
+                </div>
+                <div class="panel-footer">&copy; <?php echo date("Y"); ?></div>
+            </div>
+        </div>
+    </div>
+</div>
