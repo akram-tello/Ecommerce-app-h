1d37510cd9dad5ac6f04a869702311abece8350b
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-24 18:45:02 +0800

    added bootstrap and jQuery files

--- layouts/common/Footer.php
+++ layouts/common/Footer.php
@@ -1,2 +1,4 @@
+<script src="../../assets/js/jquery2.js"></script>
+<script src="../../assets/js/bootstrap.min.js"></script>
 </body>
 </html>
--- layouts/common/Header.php
+++ layouts/common/Header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>E-book Store</title>
+		<link rel="stylesheet" href="../../assets/css/bootsrap/bootstrap.min.css"/>
 	</head>
 <body>
 
