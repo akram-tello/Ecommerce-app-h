09f44f593ac4ae973df3a3d5f9024f2018c40f6a
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-25 17:34:12 +0800

    added cart page

---
+++ cart.php
@@ -0,0 +1,39 @@
+<?php
+require "database/config/constants.php";
+?>
+
+<?php
+// include Header.php file
+include ('layouts/common/Header.php');
+?>
+
+<?php
+// include _loader.php file
+include ('layouts/components/_loader.php');
+?>
+
+<?php
+// include cart_body.php file
+include ('layouts/components/_cart_body.php');
+?>	
+
+<?php
+// include Footer.php file
+include ('layouts/common/Footer.php');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
---
+++ layouts/components/_cart_body.php
@@ -0,0 +1,43 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            <a href="../index.php" class="navbar-brand">E-book Store</a>
+        </div>
+
+    </div>
+</div>
+
+	<div class="container-fluid" style="margin-top: 5em;">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="cart_msg">
+				<!--Cart Message--> 
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Cart Checkout</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-2 col-xs-2"><b>Action</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Image</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Name</b></div>
+							<div class="col-md-2 col-xs-2"><b>Quantity</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Price</b></div>
+							<div class="col-md-2 col-xs-2"><b>Price in <?php echo CURRENCY; ?></b></div>
+						</div>
+						<div id="cart_checkout"></div>
+						</div> 
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+		</div>
+
+<script>var CURRENCY = '<?php echo CURRENCY; ?>';</script>
