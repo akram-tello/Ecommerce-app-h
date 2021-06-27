32d716f56dd748334581a24d0b2b9fae886cdaec
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-27 19:57:25 +0800

    added API to remove/update cart item

---
+++ controllers/remove_item.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$ip_add = getenv("REMOTE_ADDR");
+include "../database/db_connection.php";
+
+//Remove Item From cart
+if (isset($_POST["removeItemFromCart"])) {
+	$remove_id = $_POST["rid"];
+	if (isset($_SESSION["uid"])) {
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND user_id = '$_SESSION[uid]'";
+	}else{
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND ip_add = '$ip_add'";
+	}
+	if(mysqli_query($con,$sql)){
+		echo "<div class='alert alert-danger'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>Product is removed from cart</b>
+				</div>";
+		exit();
+	}
+}
+
+
+?>
---
+++ controllers/update_item.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$ip_add = getenv("REMOTE_ADDR");
+include "../database/db_connection.php";
+
+//Update Item From cart
+if (isset($_POST["updateCartItem"])) {
+	$update_id = $_POST["update_id"];
+	$qty = $_POST["qty"];
+	if (isset($_SESSION["uid"])) {
+		$sql = "UPDATE cart SET qty='$qty' WHERE p_id = '$update_id' AND user_id = '$_SESSION[uid]'";
+	}else{
+		$sql = "UPDATE cart SET qty='$qty' WHERE p_id = '$update_id' AND ip_add = '$ip_add'";
+	}
+	if(mysqli_query($con,$sql)){
+		echo "<div class='alert alert-info'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>Product is updated</b>
+				</div>";
+		exit();
+	}
+}
+
+?>
