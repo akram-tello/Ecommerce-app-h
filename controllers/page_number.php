3c1ed8b689f1e4f834eacd1b7fd13d8f1e297a99
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-06-27 19:58:13 +0800

    added API to get page number

---
+++ controllers/page_number.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$ip_add = getenv("REMOTE_ADDR");
+include "../database/db_connection.php";
+
+if(isset($_POST["page"])){
+	$sql = "SELECT * FROM products";
+	$run_query = mysqli_query($con,$sql);
+	$count = mysqli_num_rows($run_query);
+	$pageno = ceil($count/9);
+	for($i=1;$i<=$pageno;$i++){
+		echo "
+			<li><a href='#' page='$i' id='page'>$i</a></li>
+		";
+	}
+}
+
+?>
